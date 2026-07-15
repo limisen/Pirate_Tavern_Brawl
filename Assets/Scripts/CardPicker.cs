fc0a5e02d58a2422019edf15abf14e5e8cab2f43
Liam <Liam.cosford.03@gmail.com>
2026-07-15 20:01:37 +0200

    Worked on Cardpicker script + added that script to the prefab

--- Assets/Scripts/CardPicker.cs
+++ Assets/Scripts/CardPicker.cs
@@ -1,16 +1,47 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CardPicker : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private bool onCardSelect = false;
+    private Vector3 startLocalScale;
+    private Vector3 startLocalPosition;
+    private Quaternion startLocalRotation;
+
+    void OnMouseDown()
     {
-        
+        if (onCardSelect == false)
+        {
+            onCardSelect = true;
+            Debug.Log("Mouse clicked on: "+ gameObject.name);
+            startLocalScale = transform.localScale;
+            startLocalPosition = transform.localPosition;
+            startLocalRotation = transform.localRotation;
+            cardManipulation();
+        }
+    }
+    private void OnMouseUp()
+    {
+        onCardSelect = false;
+        cardManipulation();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        cardManipulation();
+    }
+    void cardManipulation()
+    {
+        if (onCardSelect == true)
+        {
+            gameObject.transform.localPosition = new Vector3(Mouse.current.position.ReadValue().x ,Mouse.current.position.ReadValue().x, 1);
+            gameObject.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+        }
+        else if (onCardSelect == false)
+        {
+            gameObject.transform.localScale = startLocalScale;
+            gameObject.transform.localPosition = startLocalPosition;
+            gameObject.transform.localRotation = startLocalRotation;
+        }
     }
 }
--- Assets/Scripts/GameLoop.cs
+++ Assets/Scripts/GameLoop.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class GameLoop : MonoBehaviour
 {
-   
+    public int coins_Available;
+    public int costs_Of_Cards;
+    public int player_HP;
+    public int opponent_HP;
+    
+    public void GameLoop_Method()
+    {
+        while (player_HP >= 1 | opponent_HP >= 1)
+        {
+           
+        }
+    }
 }
