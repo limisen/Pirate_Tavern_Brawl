b6378345ed47b44625660fec74519e3c509359be
Liam <Liam.cosford.03@gmail.com>
2026-07-15 14:48:11 +0200

    Started defining the card class (script) and created a GameManager (script)

--- Assets/Scripts/CardClass.cs
+++ Assets/Scripts/CardClass.cs
@@ -2,15 +2,35 @@ using UnityEngine;
 
 public class CardClass : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public enum Conditions 
     {
-        
+        none, No_Attack
     }
+    public string Name_Of_Card;
+    public int Cost_of_Card;
+    public string Card_Description;
+    public Sprite Card_Portrait;
+    public Conditions condition;
 
-    // Update is called once per frame
-    void Update()
+    public class Attack : CardClass
     {
-        
+        public int Damage;
+
+        public Attack(int DMG, int Cost, Conditions con)
+        {
+            Damage = DMG; 
+            Cost_of_Card = Cost;
+            condition = con;
+        }
+    }
+    public class Defence : CardClass
+    {
+        public int Defence_Value;
+
+        public Defence(int Def, int Cost)
+        {
+            Defence_Value = Def; 
+            Cost_of_Card = Cost;
+        }
     }
 }
