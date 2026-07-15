8f722fa40b771a7d278b7817991c820ab7c23b32
Liam <Liam.cosford.03@gmail.com>
2026-07-15 18:31:31 +0200

    Added some scritps in preperation for what may be needed + tidied up some empty lines in "OnMouseHover"

--- Assets/Scripts/OnMouseHover.cs
+++ Assets/Scripts/OnMouseHover.cs
@@ -3,32 +3,19 @@ using UnityEngine;
 public class OnMouseHover : MonoBehaviour
 {
     private bool onCard = false;
+    private Vector3 startLocalScale;
     private Vector3 startLocalPosition;
     private Quaternion startLocalRotation;
-    private Vector3 startLocalScale;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
 
     void OnMouseOver()
     {
-        if (!onCard)
+        if (onCard == false)
         {
             onCard = true;
-            Debug.Log("Mouse is over gameObject"+ gameObject.name);
+            //Debug.Log("Mouse is over gameObject"+ gameObject.name);
+            startLocalScale = transform.localScale;
             startLocalPosition = transform.localPosition;
             startLocalRotation = transform.localRotation;
-            startLocalScale = transform.localScale;
             cardInformation();
         }
     }
@@ -36,21 +23,17 @@ public class OnMouseHover : MonoBehaviour
     void OnMouseExit()
     {
         onCard = false;
-        Debug.Log("Mouse is not over gameObject");
+        //Debug.Log("Mouse is not over gameObject");
         cardInformation();
-        
     }
 
     void cardInformation()
     {
         if (onCard == true)
         {
-
             gameObject.transform.localScale = new Vector3(1.5f, 1.0f, 1);
             gameObject.transform.localPosition = new Vector3(0, 0.34f, 0);
             gameObject.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
-            
-
         }
         else if (onCard == false)
         {
