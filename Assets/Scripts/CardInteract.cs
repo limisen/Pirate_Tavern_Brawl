27c72a4b0eb56bc350dbbeda3d69ad71b8e02a3d
Liam <Liam.cosford.03@gmail.com>
2026-07-20 10:52:42 +0200

    Combined CardPicker And onMouseHover to one script

---
+++ Assets/Scripts/CardInteract.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CardInteract : MonoBehaviour
+{
+    private bool CardSelected = false;
+    private bool onCard = false;
+    private Vector3 startLocalScale;
+    private Vector3 startLocalPosition;
+    private Quaternion startLocalRotation;
+    private int cardRendererStartingValue;
+
+    private Renderer cardRenderer;
+    private Camera cam;
+
+    private void Start()
+    {
+        cam = Camera.main;                              // Define the camera we are viewing through
+        startLocalScale = transform.localScale;         // Set the starting values so we have something to return to after manipulating the cards
+        startLocalPosition = transform.localPosition;
+        startLocalRotation = transform.localRotation;
+
+        cardRenderer = GetComponent<SpriteRenderer>();
+        cardRendererStartingValue = cardRenderer.sortingOrder;
+    }
+    void OnMouseDown()
+    {
+        if (CardSelected == false)                        // If we click on a card set "CardSelected" to true and "onCard" to false.
+        {
+            CardSelected = true;                          // Attempting to make it so we dont try to both enlarge the card and move it at the same time
+            onCard = false;
+        }
+    }
+    private void OnMouseUp()
+    {
+        CardSelected = false;
+        cardManipulation();
+    }
+    void OnMouseOver()
+    {
+        if (CardSelected == false && onCard == false)
+        {
+            onCard = true;
+            cardManipulation();
+        }
+    }
+    void OnMouseExit()
+    {
+        onCard = false;
+        cardManipulation();
+    }
+    private void Update()
+    {
+        if (CardSelected == true && onCard == false)
+        {
+            cardManipulation();                        // if we are holding down M1 then move the card along with the mouse
+        }
+    }
+
+    void cardManipulation()
+    {
+        if (CardSelected == true)     // If we are holding down M1, rotate the card upright then move it along with the mouse
+        {
+            gameObject.transform.localScale = new Vector3(0.3f, 0.3f, 1);
+            gameObject.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            gameObject.transform.localPosition = cam.ScreenToWorldPoint(new Vector3((Mouse.current.position.ReadValue().x - 35), (Mouse.current.position.ReadValue().y + 350), 11));
+            cardRenderer.sortingOrder = 100;
+        }
+        else if (onCard == true)    // If we are hovering over a card, Enlarge it and rotate it upright
+        {
+            gameObject.transform.localScale = new Vector3(1f, 1f, 1);
+            gameObject.transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            gameObject.transform.localPosition = new Vector3(0, 2.5f, 0);
+            cardRenderer.sortingOrder = 100;
+        }
+        else if (CardSelected == false || onCard == false) // if we arent hovering over a card or holding down M1 on it, return it to the scale, rotation and position it was at the start.
+        {
+            gameObject.transform.localScale = startLocalScale;
+            gameObject.transform.localRotation = startLocalRotation;
+            gameObject.transform.localPosition = startLocalPosition;
+            cardRenderer.sortingOrder = cardRendererStartingValue;
+        }
+    }
+}
