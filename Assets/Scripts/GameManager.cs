ed7620dce73f87626e1c9bcd763131f360375eeb
Liam <Liam.cosford.03@gmail.com>
2026-07-21 08:46:44 +0200

    Adjusted cardInstantiation so that they now get created under the assigned parent (via the serializedField)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,6 +4,7 @@ using static CardClass;
 public class GameManager : MonoBehaviour
 {
     GameLoop Encounter_Loop;
+    [SerializeField] GameObject ParentObject;
     [SerializeField] GameObject AttackCardPrefab;
     [SerializeField] GameObject DefenceCardPrefab;
     [SerializeField] GameObject SpecialCardPrefab;
@@ -13,17 +14,19 @@ public class GameManager : MonoBehaviour
     public int player_HP = 30;
     public int opponent_HP = 30;
 
-    private void Main()
+    private void Start()
     {
         // start of Encounter
-
+        coins_Available = 50; 
+        costs_Of_Card = 3;
+        player_HP = 30;
+        opponent_HP = 30;
         // Create the list of cards
-
         for (int i = 0; i < 8; i++)
         {
-            if (i <= 3)
+            if (i <= 2)
             {
-                GameObject Card = Instantiate(AttackCardPrefab);
+                GameObject Card = Instantiate(AttackCardPrefab,ParentObject.transform);
 
                 AttackCard attackCard = Card.GetComponent<AttackCard>();
                 attackCard.Name_Of_Card = "Attack_Card_" + i.ToString();
@@ -35,27 +38,35 @@ public class GameManager : MonoBehaviour
 
                 attackCard.Card_Description = "blabla bla";
 
+                attackCard.Damage = Random.Range(1,4);
+
                 attackCard.condition = Conditions.none;
             }
-            else if (i > 3 && i <= 6)
+            else if (i > 2 && i <= 5)
             {
-                GameObject Card = Instantiate(DefenceCardPrefab);
+                GameObject Card = Instantiate(DefenceCardPrefab,ParentObject.transform);
 
                 DefenceCard defenceCard = Card.GetComponent<DefenceCard>();
                 defenceCard.Name_Of_Card = "Defence_Card_" + i.ToString();
                 Card.name = defenceCard.Name_Of_Card;
 
+                defenceCard.Cost_of_Card = costs_Of_Card;
+
                 defenceCard.Card_Description = "Bla Bla Bla";
+
+                defenceCard.Defence_Value = Random.Range(1, 4);
 
                 defenceCard.condition = Conditions.none;
             }
-            else if (i > 6 && i <= 8)
+            else if (i > 5 && i <= 8)
             {
-                GameObject Card = Instantiate(SpecialCardPrefab);
+                GameObject Card = Instantiate(SpecialCardPrefab,ParentObject.transform);
 
                 SpecialCard specialCard = Card.GetComponent<SpecialCard>();
-                specialCard.Name_Of_Card = "Defence_Card_" + i.ToString();
+                specialCard.Name_Of_Card = "Special_Card_" + i.ToString();
                 Card.name = specialCard.Name_Of_Card;
+
+                specialCard.Cost_of_Card = costs_Of_Card;
 
                 specialCard.Card_Description = "Bla Bla Bla";
 
