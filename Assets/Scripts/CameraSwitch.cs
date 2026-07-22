750e708fba352df1fa8059fcdb6dfc451b0af877
Pastamacka <cohlsson4@gmail.com>
2026-07-22 14:41:04 +0200

    Conditions for hide buyButtons

--- Assets/Scripts/CameraSwitch.cs
+++ Assets/Scripts/CameraSwitch.cs
@@ -1,11 +1,19 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class CameraSwitch : MonoBehaviour
 {
     [SerializeField] Camera camera_TableView;
     [SerializeField] Camera camera_TopDown;
     [SerializeField] Camera camera_BarView;
+    [SerializeField] GameObject BarButtons;
+    UserInterface userInterface;
 
+    public string currentCamera = "TableCamera";
+    public void Start()
+    {
+        userInterface = GetComponent<UserInterface>();
+    }
     public void SwitchToCamera(string CameraToSwitchTo = "TableView")
     {
         if (CameraToSwitchTo == "TableView")
@@ -14,6 +22,7 @@ public class CameraSwitch : MonoBehaviour
             camera_BarView.enabled = false;
 
             camera_TableView.enabled = true;
+            currentCamera = "TableCamera";
         }
         else if (CameraToSwitchTo == "TopDown")
         {
@@ -21,6 +30,8 @@ public class CameraSwitch : MonoBehaviour
             camera_BarView.enabled = false;
 
             camera_TopDown.enabled = true;
+            currentCamera = "TopCamera";
+            BarButtons.gameObject.SetActive(false);
         }
         else if (CameraToSwitchTo == "BarView")
         {
@@ -28,6 +39,7 @@ public class CameraSwitch : MonoBehaviour
             camera_TableView.enabled = false;
 
             camera_BarView.enabled = true;
+            currentCamera = "BarCamera";
         }
     }
 }
